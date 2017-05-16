138d0694722aeebc6d92531c2850dfb4775105e5
begimov <begimov@gmail.com>
2017-05-16 17:21:40 +0300

    migrations, models and basic relationships added

---
+++ app/Permissions/HasPermissionsTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Permissions;
+
+use App\Role;
+use App\Permission;
+
+/**
+ *
+ */
+trait HasPermissionsTrait
+{
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'users_roles');
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'users_permissions');
+    }
+}
