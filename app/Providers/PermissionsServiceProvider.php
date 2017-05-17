9b99a5e7cf590a9e5074a92a157d6d0c5ff611fa
begimov <begimov@gmail.com>
2017-05-17 16:59:06 +0300

    custom blade directive added to PermissionsServiceProvider and tested within templates

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Gate;
 use App\Permission;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class PermissionsServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,15 @@ class PermissionsServiceProvider extends ServiceProvider
               Gate::define($permission->name, function ($user) use ($permission) {
                   return $user->hasPermissionTo($permission);
               });
+          });
+
+          // registering custom blade directives
+          Blade::directive('role', function ($role) {
+              return "<?php if (auth()->check() && auth()->user()->hasRole({$role})): ?>";
+          });
+
+          Blade::directive('endrole', function ($role) {
+              return '<?php endif; ?>';
           });
     }
 
